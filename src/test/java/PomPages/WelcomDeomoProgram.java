176da93d875faedf012103b81512bb4f01235a57
Tejas-deshmukh24 <tejasdeshmukh11223@gmail.com>
2025-07-18 18:04:47 +0530

    pushfilegit

---
+++ src/test/java/PomPages/WelcomDeomoProgram.java
@@ -0,0 +1,12 @@
+package PomPages;
+
+public class WelcomDeomoProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to gitbash");
+
+	}
+
+}
